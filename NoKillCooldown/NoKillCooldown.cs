ae61de8d58847530f2384160007731cab2022165
sv_du <52849920+sv-du@users.noreply.github.com>
2024-12-17 20:01:29 -0800

    Upload

---
+++ NoKillCooldown/NoKillCooldown.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Reactor;
+
+namespace NoKillCooldown;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+public partial class NoKillCooldown : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+    public static BepInEx.Logging.ManualLogSource log;
+
+    public override void Load()
+    {
+        log = Log;
+        log.LogMessage("No Kill Cooldown Mod has loaded");
+        Harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.SetKillTimer))]
+    public static class KillTimerPatch
+    {
+        public static void Prefix([HarmonyArgument(0)] ref float time)
+        {
+            time = 0f;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# No Kill Cooldown
+
+This is a simple Among Us mod that makes imposters have no kill cooldown
+
+This doesn't work on regular lobbies, so you'd have to use your own custom lobby
